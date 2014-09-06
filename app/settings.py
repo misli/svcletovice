13b56b23778a3f0bc8935d6f0a41c810d61c4ca8
Jakub Dorňák <jdornak@redhat.com>
2014-09-06 18:40:55 +0200

    Initial Commit

---
+++ app/settings.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+
+"""
+Django settings for svcletovice project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+import os
+from django.utils.translation import ugettext_lazy as _
+from misli.utils import get_secret_key
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret_key(os.path.join(BASE_DIR, 'data', 'secret_key'))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG   = 'DEBUG'   in os.environ and os.environ['DEBUG']   and True or False
+DBDEBUG = 'DBDEBUG' in os.environ and os.environ['DBDEBUG'] and True or False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['svcletovice.cz', '127.0.0.1']
+
+ADMINS = (
+    ('Jakub Dorňák', 'admin@misli.cz'),
+)
+MANAGERS = (
+    ('Jakub Dorňák', 'admin@misli.cz'),
+    ('SVČ Letovice', 'info@ddmletovice.cz'),
+)
+SERVER_EMAIL = '"WEB SVČ Letovice" <admin@svcletovice.cz>'
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'tagging',
+    'djangocms_text_ckeditor',
+    'cms',
+    'mptt',
+    'django_mptt_admin',
+    'menus',
+    'south',
+    'sekizai',
+    'sortedm2m',
+    'photologue',
+    'cmsplugin_photologue',
+    'misli',
+    'domecek',
+    'captcha',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware',
+    'misli.middleware.SecureAdminMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+WSGI_APPLICATION = 'wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'cs'
+
+TIME_ZONE = 'Europe/Prague'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'htdocs', 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'htdocs', 'media')
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+SITE_ID = 1
+
+LANGUAGES = (
+    ('cs', _('Czech')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'conf', 'locale'),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'cms.context_processors.media',
+    'sekizai.context_processors.sekizai',
+    'misli.context_processors.misli',
+)
+
+# security settings
+SECURE_PROXY_SSL_HEADER = ('SECURE', 'true')
+SESSION_COOKIE_SECURE = not DEBUG
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+    'handlers': {
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': ['require_debug_false'],
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'mail_admins'],
+            'level': DEBUG and 'DEBUG' or 'INFO',
+            'propagate': True,
+        },
+        #'django.db.backends': {
+        #    'level': DBDEBUG and 'DEBUG' or 'INFO',
+        #    'propagate': True,
+        #},
+    },
+}
+
+
+#####################
+# CMS Configuration #
+#####################
+
+CMS_LANGUAGES = {
+    1: [
+        {
+            'code': 'cs',
+            'name': _('Czech'),
+        },
+    ],
+    'default': {
+        'public': True,
+        'fallbacks': ['cs'],
+        'hide_untranslated': True,
+        'redirect_on_fallback': True,
+    },
+}
+
+CMS_TEMPLATES = (
+    ('default.html', _('Default')),
+)
+
+CMS_PLACEHOLDER_CONF = {
+    'content': {
+        'name': _('Content'),
+    },
+}
+
---
+++ app/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from domecek.urls import club_registration
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^prihlaska/', include(club_registration, 'club_registration', 'club_registration')),
+    url(r'^captcha/', include('captcha.urls')),
+    url(r'^', include('cms.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+)
+
