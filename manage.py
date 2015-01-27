317fe04986a8c8543e9047379801c462d4afad6d
Jakub Dorňák <jdornak@redhat.com>
2015-01-27 20:44:16 +0100

    new manage.py, getdata

--- manage.py
+++ manage.py
@@ -10,16 +10,21 @@ if __name__ == "__main__":
     sys.path.insert(0, join(dirname(realpath(__file__)), "app"))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-    if os.getuid() == 0:
-        import pwd
+    if os.getuid() == 0 and 'collectstatic' not in sys.argv:
+        import pwd, grp
         user = pwd.getpwnam('django-svcletovice')
+        groups = [g.gr_gid for g in grp.getgrall() if user.pw_name in g.gr_mem]
         os.setgid(user.pw_gid)
+        os.setgroups(groups)
         os.setuid(user.pw_uid)
-        os.environ['USER']  = user.pw_name
-        os.environ['HOME']  = user.pw_dir
-        os.environ['SHELL'] = user.pw_shell
-        if os.path.exists(os.environ['HOME']):
+        os.environ['USER']      = user.pw_name
+        os.environ['LOGNAME']   = user.pw_name
+        os.environ['HOME']      = user.pw_dir
+        os.environ['SHELL']     = user.pw_shell
+        try:
             os.chdir(os.environ['HOME'])
+        except:
+            pass
 
     from django.core.management import execute_from_command_line
 
