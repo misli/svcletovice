d9406adefe8529650da680e069d125241563c332
Jakub Dorňák <jdornak@redhat.com>
2015-08-31 13:50:30 +0200

    pořádek v urls.py

--- app/svcletovice/urls.py
+++ app/svcletovice/urls.py
@@ -4,23 +4,16 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from domecek.urls import club_registration
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # url(regex, view, kwargs=None, name=None, prefix='')
+    url(r'', include('django.contrib.staticfiles.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
     url(r'^taggit_autosuggest/', include('taggit_autosuggest.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^domecek/', include('domecek.urls', 'domecek', 'domecek')),
     url(r'^captcha/', include('captcha.urls')),
     url(r'^', include('cms.urls')),
-)
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
 )
 
